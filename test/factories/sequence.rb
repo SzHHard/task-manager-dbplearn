b52d019e93161e02db002d4b6899a8c279eaf38f
Mikhail Korenkov <mikhail.korenkov@dualbootpartners.com>
2022-07-20 13:33:34 +0300

    added sequences and a test for tasks

--- test/factories/sequence.rb
+++ test/factories/sequence.rb
@@ -1,8 +1,11 @@
 FactoryBot.define do
-  sequence :string, aliases: [:first_name, :last_name, :password, :avatar] do |n|
+  sequence :string, aliases: [:first_name, :last_name, :password, :avatar, :state, :name, :description] do |n|
     "string#{n}"
   end
   sequence :email do |n|
     "person#{n}@example.com"
+  end
+  sequence :expired_at do |n|
+    "2022-07-19"
   end
 end
