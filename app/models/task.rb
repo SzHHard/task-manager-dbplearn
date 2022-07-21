ecaf28f67cff39de172c636b48248d7500c26b7f
Mikhail Korenkov <mikhail.korenkov@dualbootpartners.com>
2022-07-21 16:01:53 +0300

    fixed review issues, ran rubocop

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,34 +1,33 @@
 class Task < ApplicationRecord
-  belongs_to :author, class_name: "User"
-  belongs_to :assignee, class_name: "User", optional: true
+  belongs_to :author, class_name: 'User'
+  belongs_to :assignee, class_name: 'User', optional: true
 
-  state_machine :initial => :new_task do
-    event :new_task_to_development do
-      transition :new_task => :in_development
+  state_machine initial: :new_task do
+    state :new_task
+    state :in_development
+    state :in_qa
+    state :in_code_review
+    state :ready_for_release
+    state :released
+    state :archived
+
+    event :move_to_development do
+      transition [:new_task, :in_qa, :in_code_review] => :in_development
     end
-    event :new_task_to_archived do
-      transition :new_task => :archived
+    event :move_to_archived do
+      transition [:new_task, :released] => :archived
     end
-    event :development_to_qa do
-      transition :in_development => :in_qa
+    event :move_to_qa do
+      transition in_development: :in_qa
     end
-    event :qa_to_development do
-      transition :in_qa => :in_development
+    event :move_to_review do
+      transition in_qa: :in_code_review
     end
-    event :qa_to_review do
-      transition :in_qa => :in_code_review
+    event :move_to_ready_for_release do
+      transition in_code_review: :ready_for_release
     end
-    event :review_to_development do
-      transition :in_code_review => :in_development
-    end
-    event :review_to_ready_for_release do
-      transition :in_code_review => :ready_for_release
-    end
-    event :ready_for_release_to_released do
-      transition :ready_for_release => :released
-    end
-    event :released_to_archieved do
-      transition :released => :archived
+    event :move_to_released do
+      transition ready_for_release: :released
     end
   end
 
